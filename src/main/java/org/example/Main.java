57cc872379fe1ecc69cb667ee4f3905b5f74d23a
Ellie Noble <the.ellie.noble@gmail.com>
2026-06-06 17:53:37 -0700

    Built selection sort

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        }
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+             int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+
+            }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
+    }
+    }
+
+
